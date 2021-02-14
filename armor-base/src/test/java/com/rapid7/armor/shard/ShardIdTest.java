471514d2209bf63964d0d51f32122d4529d00192
Austin Lee <austin_lee@rapid7.com>
2021-02-14 08:08:04 -0800

    Add current test

--- armor-base/src/test/java/com/rapid7/armor/shard/ShardIdTest.java
+++ armor-base/src/test/java/com/rapid7/armor/shard/ShardIdTest.java
@@ -4,6 +4,9 @@ import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
 import org.junit.jupiter.api.Test;
+
+import com.rapid7.armor.Constants;
+
 import static com.rapid7.armor.Constants.INTERVAL_UNITS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -45,6 +48,36 @@ public class ShardIdTest {
     String table = "vulntable";
     long interval = (24 * 60 * 60 * 1000) / INTERVAL_UNITS; // 24 hours
     Instant timestamp = Instant.now(clock);
+    int shardNum = 0;
+
+    ShardId shardId = new ShardId(tenant, table, interval, timestamp, shardNum);
+
+    long expectedIntervalSlice = timestamp.toEpochMilli() / (interval * INTERVAL_UNITS);
+    Instant expectedIntervalStart = Instant.ofEpochMilli(expectedIntervalSlice * interval * INTERVAL_UNITS);
+    String expectedShardPath = String.format(
+        "%s/%s/%d/%s/%d",
+        tenant,
+        table,
+        interval,
+        expectedIntervalStart,
+        shardNum
+    );
+
+    assertEquals(tenant, shardId.getTenant());
+    assertEquals(table, shardId.getTable());
+    assertEquals(interval, shardId.getInterval());
+    assertEquals(expectedIntervalSlice, shardId.getIntervalSlice());
+    assertEquals(expectedIntervalStart, shardId.getIntervalStart());
+    assertEquals(expectedShardPath, shardId.getShardId());
+  }
+  
+  @Test
+  public void testShardIdConstructorCurrentInterval() {
+    Clock clock = Clock.fixed(Instant.parse("2021-01-01T00:15:00Z"), ZoneId.of("UTC"));
+    String tenant = "myorg";
+    String table = "vulntable";
+    long interval = Constants.MAX_INTERVAL; // 24 hours
+    Instant timestamp = Instant.now();
     int shardNum = 0;
 
     ShardId shardId = new ShardId(tenant, table, interval, timestamp, shardNum);
