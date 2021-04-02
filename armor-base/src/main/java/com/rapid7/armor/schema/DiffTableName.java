4bf8a0e0de4c3d409bf92148d5f267cefb3dd248
Austin Lee <austin_lee@rapid7.com>
2021-04-02 15:43:57 -0700

    Lower case diff table names alway

--- armor-base/src/main/java/com/rapid7/armor/schema/DiffTableName.java
+++ armor-base/src/main/java/com/rapid7/armor/schema/DiffTableName.java
@@ -7,10 +7,10 @@ public final class DiffTableName {
   private DiffTableName() {}
   
   public static String generatePlusTableDiffName(String table, Interval interval, ColumnId columnId) {
-    return table + "_" + interval.name().toLowerCase() + "_" + columnId.getName() + "_plus";
+    return table + "_" + interval.name().toLowerCase() + "_" + columnId.getName().toLowerCase() + "_plus";
   }
   
   public static String generateMinusTableDiffName(String table, Interval interval, ColumnId columnId) {
-    return table + "_" + interval.name().toLowerCase() + "_" + columnId.getName() + "_minus";
+    return table + "_" + interval.name().toLowerCase() + "_" + columnId.getName().toLowerCase() + "_minus";
   }
 }
